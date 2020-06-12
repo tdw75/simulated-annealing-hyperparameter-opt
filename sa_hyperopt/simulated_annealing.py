3e1958ddcea37145d8f97e953a3685b30f368cbe
tdw75 <tdw75@outlook.com>
2020-06-12 16:05:04 +0200

    Extract code from fit method into separate methods for increased modularity

--- sa_hyperopt/simulated_annealing.py
+++ sa_hyperopt/simulated_annealing.py
@@ -21,35 +21,56 @@ class SimulatedAnnealingSearchCV:
         self.iterations = None
         self.seed = seed
         np.random.seed(self.seed)
+        random.seed(self.seed)
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, cooling_rate: float = 0.01, initial_temperature: float = 10.0):
-        params_dict = self.params.copy()
-        params_current = {k: random.choice(v) for k, v in params_dict.items()}
-        keys = [k for k, v in params_dict.items() if len(v) > 1]
+    @staticmethod
+    def calculate_reset_threshold(initial_temperature: float, cooling_rate: float) -> int:
+        approx_iterations = int(-np.log(initial_temperature) / (np.log(1 - cooling_rate))) + 1
+        return int(approx_iterations / 5 + 0.5)
+
+    @staticmethod
+    def calculate_weights_for_param_distributions(params_dict: dict, keys: list) -> list:
         weights = [len(params_dict[k]) for k in keys]
         total_weight = sum(weights)
-        weights = [x / total_weight for x in weights]
+        return [x / total_weight for x in weights]
 
-        params_best = params_current.copy()
+    def save_best_hyperparameter_config(self, score_best: float, params_best: dict, best_estimator):
+        self.best_score_ = score_best
+        self.best_params_ = params_best.copy()
+        self.best_estimator_ = best_estimator
+
+    def reset_to_current_best(self, threshold: int):
+        params_current = self.best_params_.copy()
+        score_current = self.best_score_
+        iter_since_best = 0
+        logging.info(
+            " No global improvements in the last {} iterations. Search restarted from best configuration so far".format(
+                threshold))
+        return params_current, score_current, iter_since_best
+
+    def fit(self, X: pd.DataFrame, y: pd.Series, cooling_rate: float = 0.01, initial_temperature: float = 10.0):
+        params_current = {k: random.choice(v) for k, v in self.params.items()}
+        keys = [k for k, v in self.params.items() if len(v) > 1]
+        weights = self.calculate_weights_for_param_distributions(self.params, keys)
+
+        self.best_params_ = params_current.copy()
         self.model.set_params(**params_current)
         cv_results = cross_validate(self.model, X=X, y=y, n_jobs=self.n_jobs, cv=self.cv, scoring=self.scoring)
-        score_best = cv_results['test_score'].mean()
-        score_current = score_best
-        temperature = initial_temperature
-        best_estimator = self.best_estimator_
+        score_current = cv_results['test_score'].mean()
+        self.best_score_ = score_current
 
         past_configs = []
         past_configs.append(params_current.values())
-        approx_iterations = int(-np.log(initial_temperature) / (np.log(1 - cooling_rate))) + 1
-        no_improvement_threshold = int(approx_iterations / 5 + 0.5)
-        iter_since_best = 0
 
+        temperature = initial_temperature
+        no_improvement_threshold = self.calculate_reset_threshold(initial_temperature, cooling_rate)
+        iter_since_best = 0
         self.iterations = 0
 
         while temperature > 1:
             key = random.choices(keys, weights)[0]
             params_temp = params_current.copy()
-            params_temp[key] = update_value(params_dict[key], params_current[key])
+            params_temp[key] = update_value(self.params[key], params_current[key])
 
             if params_temp.values() in past_configs:
                 continue
@@ -64,13 +85,10 @@ class SimulatedAnnealingSearchCV:
 
             self.score_tracker.append(score_new)
             self.iterations += 1
+            print_log(score_temp, score_current, self.best_score_, score_new, prob, self.iterations)
 
-            print_log(score_temp, score_current, score_best, score_new, prob, self.iterations)
-
-            if score_temp >= score_best:
-                score_best = score_temp
-                params_best = params_new.copy()
-                best_estimator = cv_results['estimator'][0]
+            if score_temp >= self.best_score_:
+                self.save_best_hyperparameter_config(score_temp, params_new, cv_results['estimator'][0])
                 iter_since_best = 0
             else:
                 iter_since_best += 1
@@ -79,16 +97,7 @@ class SimulatedAnnealingSearchCV:
             temperature *= (1 - cooling_rate)
 
             if iter_since_best >= no_improvement_threshold:
-                params_current = params_best.copy()
-                score_current = score_best
-                iter_since_best = 0
-                logging.info(
-                    " No global improvements in the last {} iterations. Search restarted from best configuration so far".format(
-                        no_improvement_threshold))
-
-        self.best_score_ = score_best
-        self.best_params_ = params_best.copy()
-        self.best_estimator_ = best_estimator
+                params_current, score_current, iter_since_best = self.reset_to_current_best(no_improvement_threshold)
 
         logging.info(" Best score found was {:.4f}".format(-self.best_score_))
 
@@ -118,7 +127,8 @@ def print_log(score_temp, score_current, score_best, score_new, prob, iterations
         logging.info(" Iteration {}: Local improvement from {:.4f} to {:.4f}, parameters updated".format(
             iterations, score_current, score_temp))
         if score_temp > score_best:
-            logging.info(" -> Global improvement from {:.4f} to {:.4f}, parameters updated".format(score_best, score_temp))
+            logging.info(
+                " -> Global improvement from {:.4f} to {:.4f}, parameters updated".format(score_best, score_temp))
     elif score_new != score_current:
         logging.info(" Iteration {}: No improvement from {:.4f} to {:.4f} but parameters updated".format(
             iterations, score_current, score_temp))
